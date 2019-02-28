e723eb1add0653536aa38db7ae4c1a4e87600f6d
lilongxi <lilongxi@gitee.com>
2019-02-28 09:56:45 +0800

    fixed consloe

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import registerServiceWorker from './sw'
-import * as  Tabs from '@components/Tabs'
 
 import App from '@components/App'
 
