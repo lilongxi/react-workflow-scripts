34c910bc75b0dfbea901ef24b1a3a7848ceda9ae
lilongxi <lilongxi@gitee.com>
2019-02-28 09:44:12 +0800

    error test

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import registerServiceWorker from './sw'
+import * as Tabs from '@components/Tabs'
 
 import App from '@components/App'
-
-
 
 ReactDOM.render(
     <App />,
