731145f0b9c505f37c0f63f12f0466aebb1e7699
lilongxi <lilongxi@gitee.com>
2019-02-28 09:46:16 +0800

    error test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import registerServiceWorker from './sw'
-import * as Tabs from '@components/Tabs'
+import * as  Tabs from '@components/Tabs'
 
 import App from '@components/App'
 
